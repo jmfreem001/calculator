40e1f391ae31b00d47272dcea781f47186ca5402
Josh Freeman <jmfreem001@gmail.com>
2019-06-21 23:14:35 -0500

    Add full addition and subtraction functionality.

--- calc.js
+++ calc.js
@@ -17,19 +17,6 @@ function multiply (list) {
 	return product;
 }
 
-// function power(a, b) {
-// 	return Math.pow(a, b)
-// }
-
-// function factorial(a) {
-// 	let counter = a;
-// 	let total = 1;
-// 	while (counter > 1){
-// 		total *= counter;
-// 		counter--;
-// 	}
-// 	return total;
-// }
 
 function operate(a, b, op){
 	//takes an operator and 2 numbers and then calls a function on the numbers.
@@ -39,10 +26,11 @@ function operate(a, b, op){
 		return subtract(a,b);
 	}
 	//TODO, mmultiply and divide. 
-
 }
 
 const display = document.querySelector(".display");
+const input = document.querySelector(".input");
+
 const dot = document.querySelector("dot");
 const clear = document.getElementById("clear");
 
@@ -51,51 +39,58 @@ const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const equals = document.querySelector(".equals");
 
-numbers.forEach(number => number.addEventListener("click", updateDisplay));
-operators.forEach(operator => operator.addEventListener("click", resetDisplay));
+numbers.forEach(number => number.addEventListener("click", updateInputDisplay));
+// operators.forEach(operator => operator.addEventListener("click", resetDisplay));
 operators.forEach(operator => operator.addEventListener("click", storeInput));
 
 equals.addEventListener("click", makeCalculation);
 
 
-function updateDisplay(e){
-	display.textContent += e.target.textContent;
-	displayValue = e.target.textContent;
-	console.log(e.target);
+function updateInputDisplay(e){
+	// Displays the va;ue of a number as it is pressed. 
+	input.textContent += e.target.textContent;
+	inputValue = input.textContent;
 }
 
-function resetCalculator(){
-	resetDisplay();
-	resetValue();
-}
-
-function resetDisplay(){
-	display.textContent = "";
-}
-
-function resetValue(){
-	displayValue = "";
-}
 
 function storeInput(e){
-	displayValue = display.textContent;
+	// Move input when pressing an operation keyl to the stored data display so a new value can be input. 
+	displayValue = input.textContent;
+	display.textContent += displayValue;
 	operation = e.target.textContent;
+	display.textContent += " " + operation;
+	resetDisplay()
 }
 
 function makeCalculation(e){
-
-	result = operate(Number(displayValue), Number(display.textContent), operation);
+	// updates total and displays when equals button is pressed
+	
+	let startingValue = total === 0 ? displayValue: total;
+	result = operate(Number(startingValue), Number(inputValue), operation);
 	resetDisplay()
 	display.textContent = result;
-	//TODO figure out how to get the second value
+	//update result with the ongoing calculation
+	total = result;
+	
 }
 
 clear.addEventListener("click", resetCalculator);
 
+function resetCalculator(){
+	resetDisplay();
+	inputValue = "";
+	display.textContent ="";
+	total = 0;
+}
+
+
+function resetDisplay(){
+	input.textContent = "";
+}
+
+
 let displayValue = "";
+let inputValue = "";
 let operation = "";
+let total = 0;
 
-
-//TODO
-// Need to add a second display for diplaying calculation adn result 
-//and leave teh other and call it inputDisplay
