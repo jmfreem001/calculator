eb370ad2e60aa3023bb925376d10b95aabf14cfd
Josh Freeman <jmfreem001@gmail.com>
2019-06-23 16:05:57 -0500

    Add ability to add a decimal point.

--- calc.js
+++ calc.js
@@ -16,22 +16,39 @@ function multiply (list) {
 	});
 	return product;
 }
+function divide(list){
+	let output = list.reduce((total, value) => {
+		return total / value;
+	});
+	return output;
+}
+
 
 
 function operate(a, b, op){
 	//takes an operator and 2 numbers and then calls a function on the numbers.
+	let array = [];
+	array.push(a);
+	array.push(b);
 	if (op === "+"){
 		return add(a,b);
 	}else if(op === "-"){
 		return subtract(a,b);
+	}else if (op ==="*"){
+		return multiply(array);
+	}else if (op ==="/"){
+		if (b === 0){
+			alert("Can't divide by zero!");
+			return "error"
+		}
+		return divide(array);
 	}
-	//TODO, mmultiply and divide. 
 }
 
 const display = document.querySelector(".display");
 const input = document.querySelector(".input");
 
-const dot = document.querySelector("dot");
+const dot = document.querySelector(".dot");
 const clear = document.getElementById("clear");
 
 const buttons = document.querySelectorAll("button");
@@ -39,37 +56,49 @@ const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const equals = document.querySelector(".equals");
 
+
 numbers.forEach(number => number.addEventListener("click", buttonHandler));
 // operators.forEach(operator => operator.addEventListener("click", resetDisplay));
 operators.forEach(operator => operator.addEventListener("click", buttonHandler));
 
-//TODO update handlers to get the string value from them so they can be passed into a generic function
-//that can proces inputs from keyboard or mouse clicks. 
 
 window.addEventListener("keydown", keyboardHandler);
 
 equals.addEventListener("click", makeCalculation);
 
+//TODO Add decimal point button handlers. 
+dot.addEventListener("click", buttonHandler);
+// Add a flag to mark when the keyboard input should not use the decimal. 
+canDot = true;
+// TODOAdd a backspace button. 
+
+//TODO make it look nice with CSS. 
+
 operatorArray = ["+", "-","/","*"];
 
 function keyboardHandler(e){
-
-	if (e.key >= 0 && e.key < 10){
+	e.preventDefault();
+	if (e.key >= 0 && e.key < 10 || e.key ==="." && canDot === true){
 		updateInputDisplay(e.key);
 	}else if(operatorArray.includes(e.key)){
-		// TODO slash not working as there is a default keybinding
-		e.preventDefault;
+		e.preventDefault();
 		storeInput(e.key);
-	}else if(e.keyCode == 13){
+	}else if(e.key ==="Enter"){
 		//to prevent unintended effects when mixing button and keyboard inputs. 
 		e.preventDefault();
 		makeCalculation();
+	}else if(e.key === "c"){
+		resetCalculator();
 	}
+	// else if (e.key ==="." && canDot === true){
+	// 	updateInputDisplay(e.key);
+
+	// }
 }
 
 function buttonHandler(e){
 	value = e.target.textContent;
-	if (value >= 0 && value < 10){
+	if (value >= 0 && value < 10|| value === "."){
 		updateInputDisplay(value);
 	}else if(operatorArray.includes(value)){
 		storeInput(value);
@@ -81,11 +110,17 @@ function updateInputDisplay(value){
 	// Displays the value of a number as it is pressed. 
 	input.textContent += value;
 	inputValue = input.textContent;
+	if(value === "."){
+		dot.removeEventListener("click", buttonHandler);
+		canDot = false;
+	}
 	console.log(`Displaying input numbers`)
 }
 
 
 function storeInput(value){
+	//TODO be able to string several together and still get teh right answer. 
+
 	// Move input when pressing an operation key to the stored data display so a new value can be input. 
 	displayValue = inputValue;
 	//only add content to display content if no
@@ -100,6 +135,10 @@ function storeInput(value){
 	display.textContent += " " + operation;
 	console.log(`storing input `)
 	resetDisplay()
+	//Add decimal point button handler back in. 
+	dot.addEventListener("click", buttonHandler);
+	// Add decimal point keyboard handler back in
+	canDot = true;
 }
 
 function makeCalculation(e){
@@ -110,6 +149,11 @@ function makeCalculation(e){
 	let startingValue = total === 0 ? displayValue: total;
 	console.log(`Calculating ${startingValue} ${operation} ${inputValue}`)
 	result = operate(Number(startingValue), Number(inputValue), operation);
+	if (result === "error"){
+		resetCalculator();
+		return 
+	}
+	// TODO if result is not an int make sure to round it to X decimals
 	resetDisplay()
 	display.textContent = result;
 	console.log(`Make calculation = ${display.textContent}`);
